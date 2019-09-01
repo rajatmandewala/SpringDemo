87aa4c396231d1093d769535f856f298aff1f893
rajatmandewala <rajatmandewala@gmail.com>
2019-09-01 19:58:34 +0530

    second commit

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List; 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class EmployeeController {
 		this.employeeService = employeeService;
 	}
 
+	@CrossOrigin
 	@GetMapping
 	public List<Employee> getAllEmployeesList() {
 		return employeeService.getEmployeesList();
